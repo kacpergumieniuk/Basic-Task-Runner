48c1a8ec406968e2fff90b7256666e1334e76088
Bombsztyl <kacpergumieniuk@gmail.com>
2021-02-04 01:13:14 +0100

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Button from './Button'
+
+const Header = (props) => {
+    
+
+    return (
+        <header className='header'>
+          <h1>{props.title}</h1>
+         <Button color={props.showAdd ? 'red' : 'green'} text={props.showAdd ? 'Close' : 'Add'} onClick={props.onAdd}/>
+        </header>
+    )
+}
+
+export default Header
